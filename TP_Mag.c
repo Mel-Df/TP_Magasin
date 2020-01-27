c87340160f93f163a04ed7112963d92c539cc3da
Mel <melody.dufour@outlook.fr>
2020-01-27 13:00:02 +0100

    premiere_iteration

--- TP_Mag.c
+++ TP_Mag.c
@@ -1,7 +1,40 @@
 #include <string.h>
 #include <stdio.h>
 
-int main (){
+struct Article {
+    char *nomarticle;
+    float prix;
+    float quantité;   
+};
+
+typedef struct Article occulte; 
+typedef struct Article etrange;
+
+void paiemenent ( *bourse,){
+float bourse;
+
+bourse = 40;
+
+printf ("Vous possedez %d pieces", bourse);
+}
+
+void constitution (*article){
+ article occulte = {"Calice", 5, 1};
+ article occulte = {"Fiole noire", 2, 1};
+ article etrange = {"Doigt de troll", 10, 1};
+ article etrange = {"Oeil", 15, 1};
+};
+
+
+void afficherarticles (*article) {
+    printf ("Jetez un oeil ! Litteralement... \n");
+    printf ("%d",  )
+};
+
+
+
+void main (){
+  //strcpy(destination,source);
 
 return 0;
 }
